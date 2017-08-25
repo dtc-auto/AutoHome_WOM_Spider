682c42e91ad06db90a0060a9abb31d453a8c24f1
ge jianyu <ge jianyu@detecon.com>
2017-08-25 14:35:03 +0800

    fix rule inheritance

--- AutoHome_WOM_Spider/settings.py
+++ AutoHome_WOM_Spider/settings.py
@@ -35,7 +35,7 @@ FEED_FORMAT = 'CSV'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
--- AutoHome_WOM_Spider/spiders/WOM_URL_Spider.py
+++ AutoHome_WOM_Spider/spiders/WOM_URL_Spider.py
@@ -7,11 +7,11 @@ from lxml import etree
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from AutoHome_WOM_Spider.modules.get_models import get_type_id
-from AutoHome_WOM_Spider.items import Autohome_WOM_URL_Item
+from AutoHome_WOM_Spider.items import AutohomeWomSpiderItem
 from AutoHome_WOM_Spider.modules.Restore import get_complete_text_autohome
 
 
-class UrlSpiderSpider(scrapy.Spider):
+class UrlSpiderSpider(CrawlSpider):
     name = "WOM_URL_Spider"
 
     reqs = []
@@ -22,26 +22,20 @@ class UrlSpiderSpider(scrapy.Spider):
         reqs.append(req_0)
         reqs.append(req_1)
     start_urls = list(set(reqs))
+    # start_urls = ["http://k.autohome.com.cn/spec/16903/#pvareaid=102175"]
     xpath = {
         'post': './/div[@class="title-name name-width-01"]/a',
-        'next_page': './/div[@class="page"]/a[@class="page-item-next"]'
+        'next_page': './/a[@class="page-item-next"]'
     }
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=xpath['post']), callback='pars', follow=True),
+        Rule(LinkExtractor(restrict_xpaths=xpath['post']), callback='parse_item', follow=True),
         Rule(LinkExtractor(restrict_xpaths=xpath['next_page']))
     )
 
-
-    def parse(self, response):
-        item = Autohome_WOM_URL_Item()
+    def parse_item(self, response):
+        item = AutohomeWomSpiderItem()
         text = get_complete_text_autohome(response.text)
-        pattern_js = re.compile("\<b\>\<a href=\"\/\/(.+?)\"\ target")
-        js_list = re.findall(pattern_js, text)
-        if js_list:
-            print(js_list)
-            if js_list:
-                for a in js_list:
-                    item['CONNENT_URL'] = a
-                    print(item)
-                    #print('finish')
-                    yield item
+        item['COMMENT_URL'] = response.url
+
+
+        yield item
